32195845e0671fc960eb9953050161d36f8b4b68
Adeoba <dbtosine4real@.com>
2022-09-17 12:33:37 -0700

    main.c file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-*main -  test function that print if interger is p*ositive or negative
+*main -  test function that print if interger is p*ositive o*r negative
 *Return: 0
 */
 
 int main(void)
 {
-	int i:
+	int i;
 
 	i = 0;
-	postive_or_negative(i);
+	positive_or_negative(i);
 
 	return (0);
 }
