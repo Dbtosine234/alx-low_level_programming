8de9635d771ad71b8e7b485c8bca599ecc7fe10f
Adeoba <dbtosine4real@.com>
2022-09-17 08:41:13 -0700

    updated bugger

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-*largest_number - returns the largest of 3 numbers
+*largest_number - returns the lar*gestof 3 numbers
 *@a: first integer
 *@b: second integer
 *@c: third integer
 *Return: largest number
 */
- 
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -25,13 +25,3 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 	return (largest);
-}
-
-
-
-
-
-
-
-
-
