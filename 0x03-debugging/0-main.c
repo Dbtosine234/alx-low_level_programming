37f339ef18a89555f39b1286cd10d7d5468861d5
Adeoba <dbtosine4real@.com>
2022-09-17 04:41:12 -0700

    Debugging an error

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*main -  test function that print if interger is p*ositive or negative
+*Return: 0
+*/
+
+int main(void)
+{
+	int i:
+
+	i = 0;
+	postive_or_negative(i);
+
+	return (0);
+}
+
+
