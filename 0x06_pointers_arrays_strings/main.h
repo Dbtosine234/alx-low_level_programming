17c0e03b4c106f6a4bb5bd15765531ac149ff8fb
Adeoba <dbtosine4real@.com>
2022-09-22 00:52:36 -0700

    funtion to concatinate two string

--- 0x06_pointers_arrays_strings/main.h
+++ 0x06_pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-int _putchar(char c)
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -16,4 +16,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
-endif
+#endif
