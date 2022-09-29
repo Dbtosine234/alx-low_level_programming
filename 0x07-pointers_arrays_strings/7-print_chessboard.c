dc6e04d4da8137dc5f83574b20542c4844bfdebe
Adeoba <dbtosine4real@.com>
2022-09-29 11:36:40 -0700

    Even more pointer to pointer

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard.
+ *
+ * @a: pointer variable a
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int d, b;
+
+	for (d = 0; d < 8; d++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[d][b]);
+		}
+		_putchar('\n');
+	}
+}
